3939f36db0d6ac788af5685455a2404e72081236
Jorgezs <ulilopezromero@gmail.com>
2025-02-24 18:49:48 +0100

    c

--- inventario_textil/views/dashboard.php
+++ inventario_textil/views/dashboard.php
@@ -23,6 +23,28 @@ $usuarios = Usuario::getAll(); // Obtener todos los usuarios
     <script src="../public/script.js"></script>
 </head>
 <body>
+<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled" type="button" role="tab" aria-controls="pills-disabled" aria-selected="false" disabled>Disabled</button>
+  </li>
+</ul>
+<div class="tab-content" id="pills-tabContent">
+  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">...</div>
+  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">...</div>
+  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">...</div>
+  <div class="tab-pane fade" id="pills-disabled" role="tabpanel" aria-labelledby="pills-disabled-tab" tabindex="0">...</div>
+</div>
+
+
     <div class="container mt-4">
         <h1 class="text-center">Bienvenido, Administrador</h1>
         <div class="d-flex justify-content-end mb-4">
@@ -40,6 +62,8 @@ $usuarios = Usuario::getAll(); // Obtener todos los usuarios
             <button onclick="window.location.href='../views/create_usuario.php';" class="btn btn-success mb-2">Crear Nuevo Usuario</button>
         </div>
 
+
+        
         <!-- Tabla de productos -->
         <h2 class="h4 mb-3">Lista de Productos</h2>
         <table class="table table-striped table-bordered">
